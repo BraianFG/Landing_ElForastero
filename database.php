69057b881118fa02c44053ab7e07a067ee067b5d
BraianFG <55467665+BraianFG@users.noreply.github.com>
2022-05-28 21:39:47 -0300

    Add files via upload

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php  
+  $servername = "localhost";
+  $database = "u819747409_braianfg287";
+  $username = "u819747409_braianfg287";
+  $password = "123456789Abc";
+   
+  $conn = mysqli_connect($servername, $username, $password, $database);
+ 
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  
+  }else{
+      
+  }
+ ?>
